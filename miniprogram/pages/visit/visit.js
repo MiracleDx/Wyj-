6853966df2e38dffd9e8fc0bb52d979bde403ac2
dongx <775422441@qq.com>
2020-05-13 00:29:09 +0800

    feat: add cloud database and option(area/level) change function

--- miniprogram/pages/visit/visit.js
+++ miniprogram/pages/visit/visit.js
@@ -3,47 +3,101 @@ let app = getApp();
 Component({
   data: {
     value: '',
-    area: [
-      { text: '全部区域', value: 0 },
-      { text: '海淀区', value: 1 },
-      { text: '丰台区', value: 2 },
-    ],
-    level: [
-      { text: '全部医院', value: 0 },
-      { text: '三甲', value: 1 },
-      { text: '二甲', value: 2 },
-    ],
+    area: [],
+    level: [],
     space: [
       { text: '默认距离', value: 0 },
       { text: '最近', value: 1 },
       { text: '最远', value: 2 },
     ],
-    areaValue: 0,
-    levelValue: 0,
+    areaValue: "all",
+    levelValue: "all",
     spaceValue: 0,
     HospitalInfoWidth: 0,
     hospitalDetailHeight: 0,
-    height: 0
-  },
-
-  onChange(e) {
-    this.setData({
-      value: e.detail,
-    });
-    console.log(e.detail)
+    height: 0,
+    hospitals: [],
+    hospitals_tmp: []
   },
 
   methods: {
     selectDetail(event) {
       console.log(event)
     },
+
+    // 搜索栏change事件
+    onChangeSearch(e) {
+      this.setData({
+        value: e.detail,
+      });
+      console.log(e.detail)
+    },
+
+    // 选项卡area change事件 todo 处理距离排序
+    onChangeAreaOption(e) {
+      // 都选中全部 恢复数组
+      if (e.detail === 'all' && this.data.levelValue === 'all') {
+        this.setData({
+          hospitals: this.data.hospitals_tmp,
+          areaValue: e.detail
+        })
+        return;
+      }
+
+      let arr = this.data.hospitals_tmp
+      
+      if (e.detail !== 'all') {
+        arr = arr.filter(data => data.area === e.detail)
+      }
+
+      // 等级处理
+      if (this.data.levelValue !== 'all') {
+        arr = arr.filter(data => data.level === this.data.levelValue)
+      }
+
+      this.setData({
+        hospitals: arr,
+        areaValue: e.detail
+      })
+    },
+
+    // 选项卡level change事件 todo 处理距离排序
+    onChangeLevelOption(e) {
+      // 都选中全部 恢复数组
+      if (e.detail === 'all' && this.data.areaValue === 'all') {
+        this.setData({
+          hospitals: this.data.hospitals_tmp,
+          levelValue: e.detail
+        })
+        return;
+      }
+
+      let arr = this.data.hospitals_tmp
+
+      if (e.detail !== 'all') {
+        arr = arr.filter(data => data.level === e.detail)
+      }
+
+      // 区域处理
+      if (this.data.areaValue !== 'all') {
+        arr = arr.filter(data => data.area === this.data.areaValue)
+      }
+
+      this.setData({
+        hospitals: arr,
+        levelValue: e.detail
+      })
+    },
+
+    // 选项卡距离 change事件 todo 处理距离排序
+    onChangeSpaceOption(e) {
+      console.log(e)
+    },
   },
 
   lifetimes: {
     // 在组件实例进入页面节点树时执行
     attached() {
-      console.log("aa", app.globalData)
-      
       // 修改hospital距离
       let width = wx.getSystemInfoSync().windowWidth - 95;
       this.setData({
@@ -56,12 +110,65 @@ Component({
         height: height - 5
       });
       // 减去顶部工具栏距离
-      wx.createSelectorQuery().in(this).select(".tool").boundingClientRect().selectViewport().scrollOffset().exec((res) => height = height - res[0].height);
+      wx.createSelectorQuery()
+          .in(this)
+          .select(".tool")
+          .boundingClientRect()
+          .selectViewport()
+          .scrollOffset().exec((res) => height = height - res[0].height);
       // 减去医院详情和顶部工具栏的间隔 tab栏的距离 两个预估的hospital距离
       height = height - 10 - app.globalData.tabBarHeight - 140;
       this.setData({
         hospitalDetailHeight: height
-      })
+      });
+      
+      
+      // 获取数据库引用
+      const db = wx.cloud.database();
+      // 查询数据
+      let that = this
+      db.collection('hospital').get({
+        success(res) {
+          // 获取医院信息
+          let hospitals = []
+          for (let i = 0; i < 20; i++) {
+            res.data.forEach(data => {
+              hospitals.push(data)
+            })
+          }
+
+          // todo 计算距离
+          
+          // 设置区域
+          let area = []
+          let t = [...new Set(hospitals.map(data => data.area))]
+          t.forEach(data => {
+            area.push({
+              "text": data,
+              "value": data
+            })
+          })
+          area.unshift({"text": "全部区域", "value": "all"})
+          
+          // 设置等级
+          let level = []
+          t = [...new Set(hospitals.map(data => data.level))]
+          t.forEach(data => {
+            level.push({
+              "text": data,
+              "value": data
+            })
+          })
+          level.unshift({"text": "全部医院", "value": "all"})
+          
+          that.setData({
+            area: area,
+            level: level,
+            hospitals: hospitals,
+            hospitals_tmp: hospitals
+          })
+        }
+      });
     }
   }
 });
